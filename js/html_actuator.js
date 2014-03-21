ca94b758e9b5023ccc13167bb0caa8d293ef4af8
Jeong Yong-uk <yong@company100.com>
2014-03-21 13:47:52 +0900

    Change the winning message.

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -134,7 +134,7 @@ HTMLActuator.prototype.updateWorstScore = function (worstScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = !won ? "You win!" : "Game over!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
